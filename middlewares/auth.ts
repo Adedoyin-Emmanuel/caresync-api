be4182c2af3e8c82942cab091040cd5e81c8248e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-05 11:07:21 +0100

    Updated the useAuth middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,8 +1,21 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { response } from "../utils";
 
-const useAuth = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.headers["x-auth-token"]);
-  next();
+const useAuth = (req: any, res: any, next: NextFunction) => {
+  const token = req.headers["authorization"];
+  if (!token)
+    return response(res, 401, "You're not authorized to perform this action!");
+  try {
+    let bearer = token.split(" ")[1];
+    let jwtSecret: any = process.env.JWT_PRIVATE_KEY;
+    let decode = jwt.verify(bearer, jwtSecret);
+    req.user = decode;
+    res.user = decode;
+  } catch (error: unknown) {
+    console.log(error);
+    return response(res, 401, "You're not authorized to perform this action!");
+  }
 };
 
 export default useAuth;
--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import AuthController from "../controllers/auth.controller";
-import { loginRateLimiter } from "../middlewares/rateLimiter";
-import { loginSlowDown } from "../middlewares/rateSlowDown";
+import { useLoginRateLimiter } from "../middlewares/rateLimiter";
+import { useLoginSlowDown } from "../middlewares/rateSlowDown";
 const authRouter = express.Router();
 
-authRouter.post("/login", [loginRateLimiter, loginSlowDown], AuthController.login);
+authRouter.post(
+  "/login",
+  [useLoginRateLimiter, useLoginSlowDown],
+  AuthController.login
+);
 authRouter.post("/refresh-token", AuthController.generateRefreshToken);
 
 export default authRouter;
