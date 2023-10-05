810be504351125fa5ab701a567224a25eb98f7c1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-05 10:24:36 +0100

    Added loginRateLimiter and loginSlowDown middlwares

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import AuthController from "../controllers/auth.controller";
+import { loginRateLimiter } from "../middlewares/rateLimiter";
+import { loginSlowDown } from "../middlewares/rateSlowDown";
 const authRouter = express.Router();
 
-authRouter.post("/login", AuthController.login);
+authRouter.post("/login", [loginRateLimiter, loginSlowDown], AuthController.login);
 authRouter.post("/refresh-token", AuthController.generateRefreshToken);
 
 export default authRouter;
