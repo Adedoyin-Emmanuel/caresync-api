8a91d90338f130ca783321d01ba69dcf3cd9eb8a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-11-11 14:00:05 +0100

    Fix: Fixed cors issue

--- sockets/socket.server.ts
+++ sockets/socket.server.ts
@@ -5,15 +5,25 @@ import { SocketMessage, GlobalUser } from "../types/types";
 
 let io: Server;
 
-const allowedOrigins = [
-  "https://getcaresync.vercel.app/",
-  "http://localhost:3000",
-];
-
 const initSocket = (server: http.Server) => {
-  io = new Server(server, {
+  const io = new Server(server, {
     cors: {
-      origin: allowedOrigins,
+      origin: (origin, callback) => {
+        const allowedOriginPatterns = [
+          /https:\/\/getcaresync\.vercel\.app$/,
+          /http:\/\/localhost:3000$/,
+        ];
+
+        // Check if the origin matches any of the patterns
+        if (
+          !origin ||
+          allowedOriginPatterns.some((pattern) => pattern.test(origin))
+        ) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
       credentials: true,
     },
   });
