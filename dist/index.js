8158eff323ecde639ca34440bc4880e30fd245e9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-02-08 04:02:28 +0100

    Fixes

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,20 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const body_parser_1 = __importDefault(require("body-parser"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const cors_1 = __importDefault(require("cors"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const express_1 = __importDefault(require("express"));
-require("express-async-errors");
-const morgan_1 = __importDefault(require("morgan"));
-const middlewares_1 = require("./middlewares/");
-const routes_1 = require("./routes");
-const utils_1 = require("./utils");
-const http_1 = __importDefault(require("http"));
-const socket_server_1 = require("./sockets/socket.server");
-dotenv_1.default.config();
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import { useErrorHandler, useNotFound, useRateLimiter } from "./middlewares/";
+import { appointmentRouter, authRouter, hospitalRouter, reviewRouter, userRouter, roomRouter, } from "./routes";
+import { connectToDb } from "./utils";
+import http from "http";
+import { initSocket } from "./sockets/socket.server";
+dotenv.config();
 const PORT = process.env.PORT || 2800;
-const app = (0, express_1.default)();
-const server = http_1.default.createServer(app);
-(0, socket_server_1.initSocket)(server);
+const app = express();
+const server = http.createServer(app);
+initSocket(server);
 //middlewares
 const allowedOriginPatterns = [
     /https:\/\/getcaresync\.vercel\.app$/,
@@ -40,22 +35,22 @@ const corsOptions = {
     },
     credentials: true,
 };
-app.use((0, cors_1.default)(corsOptions));
-app.use((0, cookie_parser_1.default)());
-app.use(body_parser_1.default.json({ limit: "100mb" }));
-app.use((0, morgan_1.default)("dev"));
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use(middlewares_1.useRateLimiter);
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(useRateLimiter);
 //endpoints
-app.use("/api/auth", routes_1.authRouter);
-app.use("/api/user", routes_1.userRouter);
-app.use("/api/hospital", routes_1.hospitalRouter);
-app.use("/api/appointment", routes_1.appointmentRouter);
-app.use("/api/review", routes_1.reviewRouter);
-app.use("/api/room", routes_1.roomRouter);
-app.use(middlewares_1.useNotFound);
-app.use(middlewares_1.useErrorHandler);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/hospital", hospitalRouter);
+app.use("/api/appointment", appointmentRouter);
+app.use("/api/review", reviewRouter);
+app.use("/api/room", roomRouter);
+app.use(useNotFound);
+app.use(useErrorHandler);
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    (0, utils_1.connectToDb)();
+    connectToDb();
 });
