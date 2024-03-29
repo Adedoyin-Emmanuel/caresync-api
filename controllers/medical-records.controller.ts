f6f9ff82d66ef61134c1486108e56c41d8d5f783
= <=>
2024-03-29 09:23:08 +0100

    Feat: Finished medical record

---
+++ controllers/medical-records.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+class MedicalRecordController {
+  async createMedicalRecord(req: Request, res: Response) {}
+
+  async getMedicalRecordById(req: Request, res: Response) {}
+
+  async getAllMedicalRecord(req: Request, res: Response) {}
+
+  async updateMedicalRecord(req: Request, res: Response) {}
+
+  async deleteMedicalRecord(req: Request, res: Response) {}
+}
+
+export default MedicalRecordController;
